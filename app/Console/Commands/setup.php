1d57dcdd370518739e097c0daec0ae57628ae987
Jinkai Chen <jinkai1987@gmail.com>
2020-10-16 16:27:28 +0800

    add setup command

---
+++ app/Console/Commands/setup.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\model\Admin;
+
+class setup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup:init';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '数据初始化';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Admin::create(array('name' => 'admin', 'email' => 'test@test.com', 'password' => '123123'));
+    }
+}
